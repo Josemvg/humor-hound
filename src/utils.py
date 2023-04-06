a1960ed93e11c1a28522570ba32228d748bf7461
José Manuel Vega Gradit <67061927+Josemvg@users.noreply.github.com>
2023-04-06 19:40:37 +0200

    Added new pre-rocessing methods to utils

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 import os
+import re
 import json
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 
 def load_json(file_path: str):
     """
@@ -55,3 +54,63 @@ def plot_metric_curves(epochs, train_curve, val_curve, train_color, val_color, m
     plt.legend(frameon=False, fontsize=15)
     plt.show()
     return
+
+def preprocess_text(text, contractions, stop_words, lemmatizer):
+    """
+    Preprocesses a text.
+
+    Args:
+    -------
+    text: str
+        Text to preprocess.
+    contractions: pandas.DataFrame
+        DataFrame containing contractions.
+    stopwords: list
+        List of stopwords.
+    lemmatizer: nltk.stem.WordNetLemmatizer
+        Lemmatizer.
+    language: str, optional
+        Language of the text. Defaults to "english".
+    
+    Returns:
+    -------
+    text: str
+        The preprocessed text.
+    """
+    # Convert to lowercase
+    text = text.lower()
+    # Expand contractions
+    text = " ".join(
+        [contractions[contractions["contraction"] == word]["expanded"].values[0]
+            if word in contractions["contraction"].values
+            else word for word in text.split()])
+    # Remove non-alphanumeric characters
+    text = re.sub(r'[^\w\s]', '', text)
+    # Remove digits
+    text = re.sub(r'\d', '', text)
+    # Remove stop words
+    text = " ".join([word for word in text.split() if word not in stop_words])
+    # Lemmatize words
+    text = " ".join([lemmatizer.lemmatize(word) for word in text.split()])
+    return text
+
+def filter_words_by_frequency(text, word_freq, threshold=3):
+    """
+    Given a text and a dictionary with the frequency of each word,
+    return a text with the words with frequency less than 3 removed.
+
+    Parameters
+    ----------
+    text : str
+        Text to be filtered.
+    word_freq : dict
+        Dictionary with the frequency of each word.
+    
+    Returns
+    -------
+    str
+        Text with the words with frequency less than 3 removed.
+    """
+    words = text.split()
+    filtered_words = [word for word in words if word_freq.get(word, 0) > threshold]
+    return " ".join(filtered_words)
